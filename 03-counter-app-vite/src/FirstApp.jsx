976e6a8b7b97c65f8bf7e19df629f5e4561606c6
PedroMurilloArnau <Pedro.marnau@gmail.com>
2023-02-28 13:56:16 +0100

    Add first component

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,19 @@
+// export function FirstApp(){
+//     return (<h1>Hola Mundo !!!</h1>);
+// }
+
+// import React from 'react'
+
+// export default function firstApp() {
+//   return (
+//     <div>firstApp</div>
+//   )
+// }
+
+// export const FirstApp = () => <h1>Arrow Function</h1>;
+
+export const FirstApp = () => {
+    return(
+        <h1>Arrow Function</h1>
+    );
+};
