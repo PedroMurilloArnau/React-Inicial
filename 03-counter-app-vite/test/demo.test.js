2472c32250e4f3f89b10e60f69982123d0410b4f
PedroMurilloArnau <76122975+PedroMurilloArnau@users.noreply.github.com>
2023-10-22 11:00:57 +0200

    Add first test and config Jest

---
+++ 03-counter-app-vite/test/demo.test.js
@@ -0,0 +1,14 @@
+test('Esta prueba no debe de fallar',()=>{
+
+    //1. Inicializacion
+    const msg1 = 'Hola Mundo';
+
+
+    //2. Estimulo
+    const msg2 = msg1.trim();
+
+
+    //3. Observar el comportamiento
+    expect( msg1 ).toBe(msg2);
+
+})
